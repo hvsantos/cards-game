67146a9a1f41eae468ec1a1a35e4f60c69825ac9
Henrique Santos <hv_santos@outlook.com>
2022-03-15 16:50:34 -0300

    create the list_card array and addcard function

---
+++ js/cards.js
@@ -0,0 +1,33 @@
+const list_card = [
+  {
+    name: 'Cartinha 1',
+    img: 'urlqtiverai',
+    attack: 80,
+    magic: 70,
+    defense: 50
+  },
+  {
+    name: 'Cartinha 2',
+    img: 'urlqtiverai',
+    attack: 80,
+    magic: 70,
+    defense: 50
+  },
+  {
+    name: 'Cartinha 3',
+    img: 'urlqtiverai',
+    attack: 80,
+    magic: 70,
+    defense: 50
+  }
+]
+
+function addCard() {
+  list_card.push({
+    name: 'Cartinha 4',
+    img: 'urlqtiverai',
+    attack: 80,
+    magic: 70,
+    defense: 50
+  })
+}
---
+++ js/sortcards.js
@@ -0,0 +1,12 @@
+let cards_player = []
+let cards_machine = []
+// partidas = toParse(list_card.length/2)
+function sortCards() {
+  // for list_card[index]
+  // if (checkSort(index))
+  // else
+}
+function sortCards(index) {
+  // let checkPlayer = cards_player
+  // let checkMachine = cards_
+}
