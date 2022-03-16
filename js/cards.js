4e495bb59d45c8db2658bb847a256eefe3ddf9b0
Henrique Santos <hv_santos@outlook.com>
2022-03-16 19:39:17 -0300

    chage some cards values and proprieties, comment addCard function

--- js/cards.js
+++ js/cards.js
@@ -1,33 +1,35 @@
 const list_card = [
   {
-    name: 'Cartinha 1',
-    img: 'urlqtiverai',
-    attack: 80,
-    magic: 70,
-    defense: 50
+    name: 'Axe',
+    img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/2/23/Axe_icon.png/',
+    attack: 3,
+    magic: 1,
+    defense: 6
   },
   {
-    name: 'Cartinha 2',
-    img: 'urlqtiverai',
-    attack: 80,
-    magic: 70,
-    defense: 50
-  },
-  {
-    name: 'Cartinha 3',
-    img: 'urlqtiverai',
-    attack: 80,
-    magic: 70,
-    defense: 50
+    name: 'Storm Spirit',
+    img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/1/13/Storm_Spirit_icon.png',
+    attack: 4,
+    magic: 5,
+    defense: 1
   }
+  // ,
+  // {
+  //   name: 'Cartinha 3',
+  //   img: 'urlqtiverai',
+  //   attack: 80,
+  //   magic: 70,
+  //   defense: 50
+  // }
 ]
 
-function addCard() {
-  list_card.push({
-    name: 'Cartinha 4',
-    img: 'urlqtiverai',
-    attack: 80,
-    magic: 70,
-    defense: 50
-  })
-}
+// function addCard(name, img, attack, magic, defense) {
+//   list_card.push({
+//     name: name,
+//     img: img,
+//     attack: attack,
+//     magic: magic,
+//     defense: defense
+//   })
+// }
+// addCard('Carta Name', 'Carta URL', 44, 44, 44)
