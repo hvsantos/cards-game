27f21bdbecbd7253aa4ae61af09cadded98d00f1
Henrique Santos <hv_santos@outlook.com>
2022-03-17 14:28:50 -0300

    add new cards

--- js/cards.js
+++ js/cards.js
@@ -3,8 +3,8 @@ const list_card = [
     name: 'Axe',
     img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/2/23/Axe_icon.png/',
     attack: 3,
-    magic: 1,
-    defense: 6
+    magic: 2,
+    defense: 5
   },
   {
     name: 'Storm Spirit',
@@ -12,15 +12,28 @@ const list_card = [
     attack: 4,
     magic: 5,
     defense: 1
+  },
+  {
+    name: 'Magnus',
+    img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/b/ba/Magnus_icon.png',
+    attack: 4,
+    magic: 3,
+    defense: 3
+  },
+  {
+    name: 'Sven',
+    img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/1/1b/Sven_icon.png',
+    attack: 6,
+    magic: 1,
+    defense: 3
+  },
+  {
+    name: 'Ember Spirit',
+    img: 'https://static.wikia.nocookie.net/dota2_gamepedia/images/9/91/Ember_Spirit_icon.png',
+    attack: 6,
+    magic: 3,
+    defense: 1
   }
-  // ,
-  // {
-  //   name: 'Cartinha 3',
-  //   img: 'urlqtiverai',
-  //   attack: 80,
-  //   magic: 70,
-  //   defense: 50
-  // }
 ]
 
 // function addCard(name, img, attack, magic, defense) {
